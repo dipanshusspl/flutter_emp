513c3b41561d42cfb92ef4056c5222665510e739
dipanshusspl <dipanshu.raj@sparrowsoftech.net>
2026-03-28 11:51:22 +0530

    message

--- backend/employees/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Employee
-from .serializers import EmployeeSerializer
- 
-class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset           = Employee.objects.all().order_by('-created_at')
-    serializer_class   = EmployeeSerializer
